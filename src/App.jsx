b042a67f880ca0f3ab99bc2ebea48f4d80ba6e4f
RAO <raorao1011@gmail.com>
2021-03-14 20:53:04 +0900

    react2

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useCallback, useMemo } from "react";
+import { CihldArea } from "./ChildArea";
+import React from "react";
+
+export default function App () {
+    const [text, setText] = useState("");
+    const [open, setOpen] = useState(false);
+
+    const onChangeText = (e) => setText(e.target.value);
+
+    const onClickOpen = () => setOpen(!open);
+
+    const onClickClose = useCallback(() => setOpen(false), []);
+
+    return (
+        <div className="App">
+            <input value={text} onChange={onChangeText}/>
+            <br/>
+            <br/>
+            <button onClick={onClickOpen}>表示</button>
+            <CihldArea open={open} onClickClose={onClickClose}/>
+        </div>
+    );
+}
